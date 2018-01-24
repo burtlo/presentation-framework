0bf588641146dec8039751b9c4c1257f5398f7be
Franklin Webber <franklin.webber@gmail.com>
2018-01-24 08:26:45 -0600

    Adds config and habitat packaging
    Signed-off-by: Franklin Webber <franklin@chef.io>

---
+++ habitat/README.md
@@ -0,0 +1,9 @@
+# Habitat package: learnchef-lite
+
+## Description
+
+Provide a brief description of the `learnchef-lite` plan / purpose.
+
+## Usage
+
+Describe the general usage for the `learnchef-lite` plan
---
+++ habitat/plan.sh
@@ -0,0 +1,9 @@
+pkg_name=presentation-framework
+pkg_origin=franklinwebber
+pkg_version="0.1.0"
+pkg_scaffolding="core/scaffolding-ruby"
+
+declare -A scaffolding_env
+
+# Add additional variables which use runtime config values
+scaffolding_env[LC_ALL]="en_US.UTF-8"
